9286a089970cad0f1771983e3595046dee0e76de
Aidan <your@email.com>
2019-07-16 23:19:42 +0000

    made counter responsive and changed color when negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  console.log("done");
+  // $(".new-tweet").on("focus", function() {
+  //   console.log("ok");
+  // });
+ 
+});
+
+
+// $("#input-text").on("keypress", () => {
+//   console.log(this.value);
+// });
+
+$("#input-text").keyup(
+  function(event) {
+    let count = 140;
+    count -= this.value.length;
+    let elm = $(event.target);
+    elm.parents('.new-tweet').find('.counter').html(count);
+    if (count < 0) {
+      elm.parents('.new-tweet').find('.counter').css('color', 'red');
+    }
+  });
+
+/*
+  (event) => {
+  let count = 140;
+  count -= this.value.length;
+  event.target.parents('.new-tweet').find('.counter').innerHTML = count;
+  console.log(value);
+  console.log(count - value.length);
+  });
+  */
