6021cfe2f011accf382c06e6bbe047ecb3f82b9a
Aidan <your@email.com>
2019-07-17 03:41:45 +0000

    added in create tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  
+  const createTweetElement = function(tweet) {
+    let $tweet = $("article").addClass("tweet")
+      .add('header')
+      .add('div')
+      .add('footer');
+    console.log($('.tweet-container'));
+    $('#tweet-container').append("<p>test</p>");
+    return $tweet;
+  };
+
+
+
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  };
+
+
+
+  //renderTweets(data);
+
+  createTweetElement();
+
+
+
+
+
+
+
+
+
+
+});
